bf4c8de7234c83101c5a3b5e677f47ea861e5208
Hashaam <hashaam1217@gmail.com>
2023-05-15 02:02:37 -0400

    Switched to OOP for the NeuralNetwork

--- ModelDefinition.cpp
+++ ModelDefinition.cpp
@@ -4,15 +4,50 @@
 #include <iostream>
 #include <cmath> 
 #include <vector>
+#include <random>
 
+//iostream is used for simple input/output
+//cmath is used to facilitate the more complex math operations being handled
+//vector is extremely important since the whole thing is built on the basis of vectors
+//random is used for it's Gausian distribution function to initialise weights and biases
 
 using namespace std;
 
 //Defining the shape of the neural network
-const int input_size = 784;
-const int hidden_layers_size = 20;
-const int output_size = 10;
+//const int input_size = 784;
+//const int hidden_layers_size = 20;
+//const int output_size = 10;
 
+class NeuralNetwork
+{
+    public:
+        //Defining a constructor
+        NeuralNetwork(const vector<int>& layer_sizes, double learning_rate) : layer_sizes_(layer_sizes_(layer_sizes), learning_rate_(learning_rate))
+    {
+        //Defining the structure of the weight matrix
+        vector<int> weight_rows(layer_sizes_.size() - 1);
+        vector<int> weight_cols(layer_sizes_.size() - 1); 
+        vector<int> bias_sizes(layer_sizes_.size() - 1);
+
+        for (int i = 0; i < layer_sizes_.size() - 1; i++)
+        {
+            weight_rows[i] = layer_sizes_[i+1]; //Number of rows in the weight matrix
+            weight_columns[i] = layer_sizes_[i]; //Number of columns in the weight matrix
+            bias_sizes[i] = layer_sizes_[i+1];
+        }
+
+        //Randomzing the values of weights and biases
+        vector<vector<double>> weights;
+        for (int i = 0; i < weight_rows.size(); i++)
+        {
+            vector<double> row(weight_rows[i]);
+            for (int j = 0; j < weight_cols[i]; j++)
+            {
+                //Need to add randomization for row in the vector
+            }
+        }
+    }
+}
 //Prototyping
 void forward_propagation();
 void backward_propagation();
