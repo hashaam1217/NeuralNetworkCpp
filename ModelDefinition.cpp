965d6363fff79e4970cfce7e9855d4e540406a29
Hashaam <hashaam1217@gmail.com>
2023-05-16 20:53:29 -0400

    Figuring out private/public variables

--- ModelDefinition.cpp
+++ ModelDefinition.cpp
@@ -20,63 +20,77 @@ using namespace std;
 
 class NeuralNetwork
 {
-
-
     public:
-        vector<int> layer_sizes_;
         double learning_rate_;
-
-
-        //Defining a constructor
-        NeuralNetwork(const vector<int>& layer_sizes, double learning_rate) : layer_sizes_(layer_sizes), learning_rate_(learning_rate)
-    {
-        //Defining the structure of the weight matrix
-        vector<int> weight_rows(layer_sizes_.size() - 1);
-        vector<int> weight_cols(layer_sizes_.size() - 1); 
-        vector<int> bias_sizes(layer_sizes_.size() - 1);
-
-        for (int i = 0; i < layer_sizes_.size() - 1; i++)
-        {
-            weight_rows[i] = layer_sizes_[i+1]; //Number of rows in the weight matrix
-            weight_cols[i] = layer_sizes_[i]; //Number of columns in the weight matrix
-            bias_sizes[i] = layer_sizes_[i+1];
-        }
-
-        //Randomzing the values of weights 
-        vector<vector<double>> weights;
-        for (int i = 0; i < weight_rows.size(); i++)
-        {
-            vector<double> row(weight_rows[i]);
-            //Randomising
-            random_device SeedGen;
-            mt19937 gen(SeedGen());
-            //Creates a normal distribution with mean = 0 and standard deviation = 1.
-            normal_distribution<double> distribution(0.0, 1.0); 
-           
-            for (int j = 0; j < weight_rows[i]; j++)
-            {
-                row[j] = distribution(gen);    
-            }
-        }
-        //Randomizing the values of biases
-        vector<vector<double>> biases;
-
-        for (int i = 0; i < bias_sizes.size(); i++)
-        {
-            vector<double> row(bias_sizes[i]);
-            //Randomising
-            random_device SeedGen;
-            mt19937 gen(SeedGen());
-            //Creates a normal distribution with mean = 0 and standard deviation = 1.
-            normal_distribution<double> distribution(0.0, 1.0);
-
-            for (int j = 0; j < bias_sizes[i]; j++)
-            {
-                row[j] = distribution(gen);
-            }
-        }
+        vector<int> layer_sizes_;
         
+        char x = 'x';
+        void print()
+        {
+            cout << learning_rate_ << endl;
+        }
+
+    private:
+        vector<vector<double>> weights_;
+        vector<vector<double>> biases_;
+ 
+    //Defining a constructor
+    NeuralNetwork(const vector<int>& layer_sizes, double learning_rate) : layer_sizes_(layer_sizes), learning_rate_(learning_rate)
+    {
+      //Defining the structure of the weight matrix
+      vector<int> weight_rows(layer_sizes_.size() - 1);
+      vector<int> weight_cols(layer_sizes_.size() - 1); 
+      vector<int> bias_sizes(layer_sizes_.size() - 1);
+
+      for (int i = 0; i < layer_sizes_.size() - 1; i++)
+      {
+          weight_rows[i] = layer_sizes_[i+1]; //Number of rows in the weight matrix
+          weight_cols[i] = layer_sizes_[i]; //Number of columns in the weight matrix
+          bias_sizes[i] = layer_sizes_[i+1];
+      }
+
+      //Randomzing the values of weights 
+      vector<vector<double>> weights_;
+      for (int i = 0; i < weight_rows.size(); i++)
+      {
+          vector<double> row(weight_rows[i]);
+          //Randomising
+          random_device SeedGen;
+          mt19937 gen(SeedGen());
+          //Creates a normal distribution with mean = 0 and standard deviation = 1.
+          normal_distribution<double> distribution(0.0, 1.0); 
+           
+          for (int j = 0; j < weight_rows[i]; j++)
+          {
+              row[j] = distribution(gen);    
+          }
+          //Adds the individual row to the final weights vector matrix
+          weights_.push_back(row);
+      }
+
+      //Randomizing the values of biases
+      vector<vector<double>> biases_;
+
+      for (int i = 0; i < bias_sizes.size(); i++)
+      {
+          vector<double> row(bias_sizes[i]);
+          //Randomising
+          random_device SeedGen;
+          mt19937 gen(SeedGen());
+          //Creates a normal distribution with mean = 0 and standard deviation = 1.
+          normal_distribution<double> distribution(0.0, 1.0);
+
+          for (int j = 0; j < bias_sizes[i]; j++)
+          {
+              row[j] = distribution(gen);
+          }
+          
+          //Adds the individual row to the final bias vector matrix
+          biases_.push_back(row);    
+      }
     }
+
+      
 };
 //Prototyping
 //void forward_propagation();
@@ -87,15 +101,7 @@ class NeuralNetwork
 
 int main(void)
 {
-
-    vector<int> Vector = {1, 2, 3};
-    NeuralNetwork MyNeuralNetwork(Vector, 3);
-    vector<int> MyVector = MyNeuralNetwork.layer_sizes_;
-    
-    for (int i = 0; i < MyVector.size(); i++)
-    {
-        cout << MyVector[i] << "\n";
-    }
-
-    cout << MyNeuralNetwork.bias_sizes;
+    vector<int> Kash = {1, 2, 3};
+    NeuralNetwork Hash(Kash, 4);
+    Hash.print();
 }
